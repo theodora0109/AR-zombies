214cae37bfcd1de0caebd16055b58443ad4c1ef5
theodora0109 <xuyuan.haowen@gmail.com>
2025-08-25 00:49:08 +0900

    Add files via upload

---
+++ Title return.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titlereturn : MonoBehaviour
+{
+     public void StartGame()
+     {
+     SceneManager.LoadScene("rger");//タイトルに戻る
+     }
+}
